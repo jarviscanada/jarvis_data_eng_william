08a4368d2f2558e5229885ec8fce22e1531146aa
William <william.wang.1484@gmail.com>
2020-03-23 20:45:38 +0000

    debug Q1

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,3 +1,4 @@
 --Group hosts by hardware info
-SELECT cpu_number, id, total_mem
-FROM host_info, total_mem DESC;
+SELECT info.cpu_number, id, info.total_mem
+FROM host_info AS info
+ORDER BY info.cpu_number, info.total_mem desc;
